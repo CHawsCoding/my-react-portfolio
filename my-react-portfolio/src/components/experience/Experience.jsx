5374cf1909f437f272cbd950a3f7f064ccb11f4f
Collin Haws <117249176+CHawsCoding@users.noreply.github.com>
2023-10-03 16:05:33 -0600

    final improvements

--- my-react-portfolio/src/components/experience/Experience.jsx
+++ my-react-portfolio/src/components/experience/Experience.jsx
@@ -1,6 +1,5 @@
 import "./Experience.css";
 import Resume from "../../assets/resume.pdf";
-import { FiArrowDown } from "react-icons/fi";
 
 function Experience() {
   // Skill pattern
@@ -8,11 +7,46 @@ function Experience() {
   return (
     <section className="experience__container" id="resume">
       <h1 className="heading__one">My Experience</h1>
+
+      <div className="proficiencies">
+        <h2 className="heading__two">Developer Proficiencies</h2>
+        <ul className="proficiencies__list">
+          <li className="proficiency__item">
+            <span className="proficiency__name">HTML</span>
+          </li>
+          <li className="proficiency__item">
+            <span className="proficiency__name">CSS</span>
+          </li>
+          <li className="proficiency__item">
+            <span className="proficiency__name">Bootstrap</span>
+          </li>
+          <li className="proficiency__item">
+            <span className="proficiency__name">Tailwind</span>
+          </li>
+          <li className="proficiency__item">
+            <span className="proficiency__name">JavaScript</span>
+          </li>
+          <li className="proficiency__item">
+            <span className="proficiency__name">Node.js</span>
+          </li>
+          <li className="proficiency__item">
+            <span className="proficiency__name">Express.js</span>
+          </li>
+          <li className="proficiency__item">
+            <span className="proficiency__name">MySQL</span>
+          </li>
+          <li className="proficiency__item">
+            <span className="proficiency__name">NoSQL</span>
+          </li>
+          <li className="proficiency__item">
+            <span className="proficiency__name">GraphQL</span>
+          </li>
+        </ul>
+      </div>
       <div>
         <a href={Resume} download className="resume__download__button">
           <button>
             <span>Download Resume</span>
-            <FiArrowDown />
           </button>
         </a>
       </div>
--- my-react-portfolio/src/components/portfolio/Portfolio.jsx
+++ my-react-portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,10 @@
 import "./Portfolio.css";
-import PortfolioImage from "../../assets/portfolio.jpg";
+import PortfolioImage from "../../assets/portfolio.png";
+import PasswordImage from "../../assets/password.png";
+import Weather from "../../assets/weather.png";
+import Trivia from "../../assets/trivia.png";
+import Readme from "../../assets/readme.png";
+import NoteTaker from "../../assets/notetaker.jpeg";
 import PortfolioCard from "./PortfolioCard";
 
 function Portfolio() {
@@ -7,12 +12,12 @@ function Portfolio() {
     <section className="portfolio__container" id="portfolio">
       <h1 className="heading__one">Portfolio</h1>
       <div className="portfolio__cards">
-        <PortfolioCard image={PortfolioImage} title={'Web Design Portfolio'} liveLink={'#'} github={'#'} />
-        <PortfolioCard image={PortfolioImage} title={'Web Design Portfolio'} liveLink={'#'} github={'#'} />
-        <PortfolioCard image={PortfolioImage} title={'Web Design Portfolio'} liveLink={'#'} github={'#'} />
-        <PortfolioCard image={PortfolioImage} title={'Web Design Portfolio'} liveLink={'#'} github={'#'} />
-        <PortfolioCard image={PortfolioImage} title={'Web Design Portfolio'} liveLink={'#'} github={'#'} />
-        <PortfolioCard image={PortfolioImage} title={'Web Design Portfolio'} liveLink={'#'} github={'#'} />
+        <PortfolioCard image={PortfolioImage} title={'First Portfolio project'} liveLink={'https://chawscoding.github.io/project-2-portfolio/'} github={'https://github.com/CHawsCoding/project-2-portfolio'} />
+        <PortfolioCard image={PasswordImage} title={'Password Generator'} liveLink={'https://chawscoding.github.io/password-generator-project/'} github={'https://github.com/CHawsCoding/password-generator-project'} />
+        <PortfolioCard image={Weather} title={'Web Design Portfolio'} liveLink={'https://chawscoding.github.io/weather-forcaster-genius/'} github={'https://github.com/CHawsCoding/weather-forcaster-genius'} />
+        <PortfolioCard image={Trivia} title={'Javascript Trivia Game'} liveLink={'https://chawscoding.github.io/js-dev-trivia/'} github={'https://github.com/CHawsCoding/js-dev-trivia'} />
+        <PortfolioCard image={NoteTaker} title={'Express Note Taker'} liveLink={'#'} github={'https://github.com/CHawsCoding/express-note-taker'} />
+        <PortfolioCard image={Readme} title={'Readme genrator'} liveLink={'#'} github={'https://github.com/CHawsCoding/the-readme-gen'} />
       </div>
     </section>
   );
