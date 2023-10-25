38c8a514c90a836303700b7ee18303118a7f3863
Collin Haws <117249176+CHawsCoding@users.noreply.github.com>
2023-10-25 14:04:24 -0600

    formatted the about me and experience pages to clean up the UI

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -12,11 +12,17 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 function Experience() {
-  // Skill pattern
-
   return (
     <section className="experience__container" id="resume">
       <h1 className="heading__one">My Experience</h1>
+
+      <div className="resume-button">
+        <a href={Resume} download className="resume__download__button">
+          <button>
+            <span>Download Resume</span>
+          </button>
+        </a>
+      </div>
 
       <div className="proficiencies">
         <h2 className="heading__two">Developer Proficiencies</h2>
@@ -26,22 +32,27 @@ function Experience() {
             <span className="proficiency__name">HTML</span>
           </li>
           <li className="proficiency__item">
-          <FontAwesomeIcon icon={faCss3} className="proficiency__icon" />
+            <FontAwesomeIcon icon={faCss3} className="proficiency__icon" />
             <span className="proficiency__name">CSS</span>
           </li>
           <li className="proficiency__item">
-          <FontAwesomeIcon icon={faBootstrap} className="proficiency__icon" />
+            <FontAwesomeIcon icon={faBootstrap} className="proficiency__icon" />
+            <span className="proficiency__name">Bootstrap</span>
           </li>
           <li className="proficiency__item">
-            <span className="proficiency__name">Tailwind</span>
-          </li>
-          <li className="proficiency__item">
-          <FontAwesomeIcon icon={faJs} className="proficiency__icon" />
+            <FontAwesomeIcon icon={faJs} className="proficiency__icon" />
             <span className="proficiency__name">JavaScript</span>
           </li>
           <li className="proficiency__item">
-          <FontAwesomeIcon icon={faNodeJs} className="proficiency__icon" />
+            <FontAwesomeIcon icon={faNodeJs} className="proficiency__icon" />
             <span className="proficiency__name">Node.js</span>
+          </li>
+          <li className="proficiency__item">
+            <FontAwesomeIcon icon={faReact} className="proficiency__icon" />
+            <span className="proficiency__name">React</span>
+          </li>
+          <li className="proficiency__item">
+            <span className="proficiency__name">Tailwind</span>
           </li>
           <li className="proficiency__item">
             <span className="proficiency__name">Express.js</span>
@@ -55,18 +66,7 @@ function Experience() {
           <li className="proficiency__item">
             <span className="proficiency__name">GraphQL</span>
           </li>
-          <li className="proficiency__item">
-          <FontAwesomeIcon icon={faReact} className="proficiency__icon" />
-            <span className="proficiency__name">React</span>
-          </li>
         </ul>
-      </div>
-      <div>
-        <a href={Resume} download className="resume__download__button">
-          <button>
-            <span>Download Resume</span>
-          </button>
-        </a>
       </div>
     </section>
   );
