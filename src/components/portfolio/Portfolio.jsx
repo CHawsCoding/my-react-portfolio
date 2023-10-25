9358a4aac54321a9a08b527b8ee0a7cdb78736ad
Collin Haws <117249176+CHawsCoding@users.noreply.github.com>
2023-10-25 15:06:58 -0600

    updated some of the projects on the portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,8 @@
 import "./Portfolio.css";
 import React from 'react';
-import PortfolioImage from "../../assets/portfolio.png";
+import PortfolioImage from "../../assets/portfolio1.png";
 import PasswordImage from "../../assets/password.png";
-import Weather from "../../assets/weather.png";
+import Diy from "../../assets/diy.png";
 import Trivia from "../../assets/trivia.png";
 import Readme from "../../assets/readme.png";
 import NoteTaker from "../../assets/notetaker.jpeg";
@@ -13,12 +13,12 @@ function Portfolio() {
     <section className="portfolio__container" id="portfolio">
       <h1 className="heading__one">Portfolio</h1>
       <div className="portfolio__cards">
-        <PortfolioCard image={PortfolioImage} title={'First Portfolio project'} liveLink={'https://chawscoding.github.io/project-2-portfolio/'} github={'https://github.com/CHawsCoding/project-2-portfolio'} />
-        <PortfolioCard image={PasswordImage} title={'Password Generator'} liveLink={'https://chawscoding.github.io/password-generator-project/'} github={'https://github.com/CHawsCoding/password-generator-project'} />
-        <PortfolioCard image={Weather} title={'Web Design Portfolio'} liveLink={'https://chawscoding.github.io/weather-forcaster-genius/'} github={'https://github.com/CHawsCoding/weather-forcaster-genius'} />
+        <PortfolioCard image={PortfolioImage} title={'React Portfolio Site (This One)'} liveLink={'http://collinhaws.com/'} github={'https://github.com/CHawsCoding/my-react-portfolio'} />
+        <PortfolioCard image={Diy} title={'Social Network for DIY Projects'} liveLink={'https://craft-house-333df780d3f8.herokuapp.com/'} github={'https://github.com/CHawsCoding/CraftHouse'} />
         <PortfolioCard image={Trivia} title={'Javascript Trivia Game'} liveLink={'https://chawscoding.github.io/js-dev-trivia/'} github={'https://github.com/CHawsCoding/js-dev-trivia'} />
         <PortfolioCard image={NoteTaker} title={'Express Note Taker'} liveLink={'#'} github={'https://github.com/CHawsCoding/express-note-taker'} />
         <PortfolioCard image={Readme} title={'Readme genrator'} liveLink={'#'} github={'https://github.com/CHawsCoding/the-readme-gen'} />
+        <PortfolioCard image={PasswordImage} title={'Password Generator'} liveLink={'https://chawscoding.github.io/password-generator-project/'} github={'https://github.com/CHawsCoding/password-generator-project'} />
       </div>
     </section>
   );
